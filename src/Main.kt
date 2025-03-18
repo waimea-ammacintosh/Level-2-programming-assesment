f8aa5e67dafd60d2b1e6a1b9e053742bd4d2e5dd
waimea-ammacintosh <ammacintosh@waimea.school.nz>
2025-03-18 15:10:11 +1300

    created title screen and instructions. added getName fun

--- src/Main.kt
+++ src/Main.kt
@@ -15,6 +15,39 @@
 
 
 fun main() {
-    println("Hello World!")
+    println("Welcome to...")
+    println(" _____ _    ______   _____ _____ _    ______ \n" +
+            "|  _  | |   |  _  \\ |  __ \\  _  | |   |  _  \\\n" +
+            "| | | | |   | | | | | |  \\/ | | | |   | | | |\n" +
+            "| | | | |   | | | | | | __| | | | |   | | | |\n" +
+            "\\ \\_/ / |___| |/ /  | |_\\ \\ \\_/ / |___| |/ / \n" +
+            " \\___/\\_____/___/    \\____/\\___/\\_____/___/  ")
+
+    println()
+    println()
+    println()
+    println("Old Gold is a game played about moving coins on a one-dimensional grid.")
+    println()
+    println("How to play:")
+    println("The aim of the game is to be the person that gets the gold coin.")
+    println("When it is your turn, you can do one of two moves:")
+    println("1. You can move one coin LEFT as many squares as you want, as long as it does not pass through or land on another coin.")
+    println("2. Take a the coin on the end (Leftmost) square. You can only do this move if there is a coin in the last square.")
+
+    println("Now, we shall commence!")
+
+
 }
 
+fun getName(prompt: String): String {
+    var userInput: String
+
+    while (true) {
+        print(prompt)
+
+        userInput = readln()
+        if (userInput.isNotBlank()) break
+
+    }
+    return userInput
+}
